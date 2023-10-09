123bb93ad2348d83886cc11d6cbc9e95c77b4fe7
Clement Li <robekeane@126.com>
2023-10-09 01:35:23 +0000

    Add ChatGPT service
    Signed-off-by: Clement Li <robekeane@126.com>

--- src/app.py
+++ src/app.py
@@ -1,12 +1,10 @@
 import gradio as gr
 import _thread as thread
 import os
-import base64
-import datetime
-import hashlib
-import hmac
 import time
 import json
+import openai
+from utils import Ws_Param
 from urllib.parse import urlparse
 import ssl
 from datetime import datetime
@@ -14,53 +12,11 @@ from time import mktime
 from urllib.parse import urlencode
 from wsgiref.handlers import format_date_time
 import websocket
+
+radio_index = 0
 answer = ""
 history_list = []
-
-class Ws_Param(object):
-
-    # 初始化
-    def __init__(self, APPID, APIKey, APISecret, Spark_url):
-        self.APPID = APPID
-        self.APIKey = APIKey
-        self.APISecret = APISecret
-        self.host = urlparse(Spark_url).netloc
-        self.path = urlparse(Spark_url).path
-        self.Spark_url = Spark_url
-        print(f'Appid: {APPID}, Apikey: {APIKey}, ApiSecret: {APISecret}, APIUrl: {Spark_url}')
-
-    # 生成url
-    def create_url(self):
-        # 生成RFC1123格式的时间戳
-        now = datetime.now()
-        date = format_date_time(mktime(now.timetuple()))
-
-        # 拼接字符串
-        signature_origin = "host: " + self.host + "\n"
-        signature_origin += "date: " + date + "\n"
-        signature_origin += "GET " + self.path + " HTTP/1.1"
-
-        # 进行hmac-sha256进行加密
-        signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
-                                 digestmod=hashlib.sha256).digest()
-
-        signature_sha_base64 = base64.b64encode(signature_sha).decode(encoding='utf-8')
-
-        authorization_origin = f'api_key="{self.APIKey}", algorithm="hmac-sha256", headers="host date request-line", signature="{signature_sha_base64}"'
-
-        authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
-
-        # 将请求的鉴权参数组合为字典
-        v = {
-            "authorization": authorization,
-            "date": date,
-            "host": self.host
-        }
-        # 拼接鉴权参数，生成url
-        url = self.Spark_url + '?' + urlencode(v)
-        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
-        return url
-
+openai.api_key = "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"
 
 # events on websocket errors ocurred
 def on_error(ws, error):
@@ -70,7 +26,6 @@ def on_error(ws, error):
 # events on websocket closed
 def on_close(ws,one,two):
     print(" ")
-
 
 # events on websocket connection
 def on_open(ws):
@@ -131,28 +86,58 @@ def gen_params(appid, domain,question):
     return data
 
 
-def greet(message, history):
-    AppID = os.environ.get('APPID')
-    APIKey = os.environ.get('APIKEY')
-    APISecret = os.environ.get('APISECRET')
-    APIUrl = os.environ.get('APIURL')
-    print(f'Appid: {AppID}, Apikey: {APIKey}, ApiSecret: {APISecret}, APIUrl: {APIUrl}')
-    wsParam = Ws_Param(AppID, APIKey, APISecret, APIUrl)
-    websocket.enableTrace(False)
-    wsUrl = wsParam.create_url()
-    ws = websocket.WebSocketApp(wsUrl, on_message=on_message, on_error=on_error, on_close=on_close, on_open=on_open)
-    ws.appid = AppID
-    history_list.append({"role": "user", "content": message})
-    ws.question = history_list
-    ws.domain = "generalv2"
+def xunfei(message, history):
+    global history_list
+    print(radio_index)
+    if radio_index == 0:
+        AppID = os.environ.get('APPID')
+        APIKey = os.environ.get('APIKEY')
+        APISecret = os.environ.get('APISECRET')
+        APIUrl = os.environ.get('APIURL')
+        print(f'Appid: {AppID}, Apikey: {APIKey}, ApiSecret: {APISecret}, APIUrl: {APIUrl}')
+        wsParam = Ws_Param(AppID, APIKey, APISecret, APIUrl)
+        websocket.enableTrace(False)
+        wsUrl = wsParam.create_url()
+        ws = websocket.WebSocketApp(wsUrl, on_message=on_message, on_error=on_error, on_close=on_close, on_open=on_open)
+        ws.appid = AppID
+        history_list.append({"role": "user", "content": message})
+        ws.question = history_list
+        ws.domain = "generalv2"
 
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
-    history_list.append({"role": "assistant", "content": answer})
-    for i in range(len(answer)):
-        time.sleep(0.05)
-        yield answer[: i+1]
+        ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        history_list.append({"role": "assistant", "content": answer})
+        for i in range(len(answer)):
+            time.sleep(0.05)
+            yield answer[: i+1]
+    else:
+        history_list.append({"role": "user", "content": message})
+        response = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo',
+            messages=history_list,
+            temperature=0.2,
+        )
+        response_msg = response.choices[0].message.content
+        history_list = history_list + [{"role":'assistant', 'content': response_msg}]
+        print(f'Msg: {response_msg}')
+        for i in range(len(response_msg)):
+            time.sleep(0.05)
+            yield response_msg[: i+1]
 
-demo = gr.ChatInterface(greet).queue()
+def change_textbox(choice):
+    print(choice)
+    global radio_index, history_list
+    radio_index = choice
+    history_list = []
+    return [], []
+    
+with gr.Blocks().queue() as demo:
+        with gr.Row():
+            with gr.Column(scale=1):
+                radio = gr.Radio(value="Xunfei", choices=["Xunfei", "ChatGPT"], type="index", label="请选择大模型服务", info="Which model?")
+            with gr.Column(scale=4):
+                uu = gr.ChatInterface(xunfei).queue()
+        radio.change(fn=change_textbox, inputs=radio, outputs=[uu.chatbot, uu.chatbot_state])
+        
     
 if __name__ == "__main__":
     demo.launch(share="False", server_name = '0.0.0.0', server_port=8888)
